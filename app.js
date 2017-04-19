861533b0dccbb1a9b0101aa49ab3dc1417c51c03
Samantha Tanner <samz_shadow@yahoo.com>
2017-04-19 16:41:50 -0600

    RPS

---
+++ app.js
@@ -0,0 +1,70 @@
+var userChoice;
+var computerChoice;
+var winner;
+
+function gameManager(){
+  compChoice()    
+  debugger
+  compareChoices()
+  displayWinner()
+}
+
+function userSelection(choice) {
+  debugger
+  userChoice = choice
+  gameManager()
+}
+
+function compChoice(){
+
+  var pickOne = Math.floor(Math.random()*10)
+  if (pickOne <4) {
+    computerChoice = "rock"
+  } else if (pickOne <7) {
+    computerChoice ="scissors"
+  }else {
+    computerChoice = "paper"
+    
+  }
+debugger
+}
+
+
+function compareChoices(){
+if(computerChoice === userChoice){
+  winner = 'TIE'
+  return;
+}else if(computerChoice === 'rock'){
+    if(userChoice === 'paper'){
+     winner = 'You papered them! :-)'
+    }else if (userChoice === 'scissors'){
+      winner = 'You were rocked. :-('
+    }
+}else if(computerChoice === 'paper'){
+    if(userChoice === 'rock'){
+     winner = 'You were papered. :-('
+    }else if (userChoice === 'scissors'){
+      winner = 'You CUT THEM! :-)'
+    }
+}else if(computerChoice === 'scissors'){
+    if(userChoice === 'rock'){
+     winner = 'You ROCKED them! :-)'
+    }else if (userChoice === 'paper'){
+      winner = 'You were cut. :-('
+    }
+
+}
+console.log("winner")
+}
+
+function displayWinner(){
+  debugger
+  document.getElementById('test').innerText = winner
+}
+
+function Reset(){
+  userChioce = ''
+  compChoice = ''
+  winner = ''
+  document.getElementById('winner-div').innterText = ''
+}
